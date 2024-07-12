e27677e776583475e0972267c2980396d51b8429
Massi1399 <massi9.1.9.3@gmail.com>
2024-07-12 19:31:46 +0200

    pass 4th stage

--- app/main.ts
+++ app/main.ts
@@ -6,8 +6,14 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
     socket.on("data" , (data)=>{
-        var path = data.toString().split(" ")[1];
-        var response = path === '/' ? "HTTP/1.1 200 OK\r\n\r\n" : "HTTP/1.1 404 Not Found\r\n\r\n";
+        var request = data.toString();
+        var path = request.split("/r/n")[0].split(" ")[1];
+        var query = request.split("/")[2];
+        var response = "HTTP/1.1 404 Not Found\r\n\r\n";
+        if(path === "/") 
+            response = "HTTP/1.1 200 OK\r\n\r\n";
+        else if(path === '/echo/${query}')
+            response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n"+ query;
         socket.write(response);
     })
    socket.on("close", () => {
